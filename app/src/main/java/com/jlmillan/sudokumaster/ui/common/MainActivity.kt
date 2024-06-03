1d47604fdcdafd3c1fd58869c3555b116381d62a
jlmillan13 <jlmillan13@gmail.com>
2024-06-03 21:30:21 +0200

    New activity_main.xml, fragment_home.xml, fragment_register.xml, loading_view.xml, MainActivity, MainFragment and nav_graph

--- app/src/main/java/com/jlmillan/sudokumaster/ui/common/MainActivity.kt
+++ app/src/main/java/com/jlmillan/sudokumaster/ui/common/MainActivity.kt
@@ -1,46 +1,103 @@
 package com.jlmillan.sudokumaster.ui.common
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jlmillan.sudokumaster.ui.theme.SudokuMasterTheme
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import com.jlmillan.sudokumaster.R
+import com.jlmillan.sudokumaster.databinding.ActivityMainBinding
+import com.jlmillan.sudokumaster.ui.common.extension.show
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SudokuMasterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setUpToolbar()
+        setContentView(binding?.root)
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(binding?.mainToolbar)
+    }
+
+    fun showToolbar(visible: Boolean) {
+        binding?.mainToolbar?.show(visible)
+    }
+
+    fun setToolbarTitle(title: String) {
+        binding?.mainToolbarTitle?.text = title
+    }
+
+    fun showLoading(loading: Boolean) {
+        binding?.mainLoading?.root?.show(loading)
+    }
+
+    fun showFavorite(active: Boolean, action: () -> Unit) {
+        with(binding?.mainToolbarStartIcon) {
+            this?.show(true, hideType = View.INVISIBLE)
+            this?.setImageResource(if (active) R.drawable.ic_favorite_fill else R.drawable.ic_favorite_border)
+            this?.setOnClickListener {
+                action()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    fun showProfile(visible: Boolean, action: () -> Unit) {
+        with(binding?.mainToolbarEndIcon) {
+            this?.show(visible, hideType = View.INVISIBLE)
+            this?.setImageResource(R.drawable.ic_account_circle)
+            this?.setOnClickListener {
+                action()
+            }
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SudokuMasterTheme {
-        Greeting("Android")
+    fun showDone(visible: Boolean, action: () -> Unit) {
+        with(binding?.mainToolbarEndIcon) {
+            this?.show(visible, hideType = View.INVISIBLE)
+            this?.setImageResource(R.drawable.ic_check)
+            this?.setOnClickListener {
+                action()
+            }
+        }
+    }
+
+    fun hideStartIcon() {
+        binding?.mainToolbarStartIcon?.show(false, hideType = View.INVISIBLE)
+    }
+
+    fun hideEndIcon() {
+        binding?.mainToolbarEndIcon?.show(false, hideType = View.INVISIBLE)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("LIFECYCLE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+        Log.e("LIFECYCLE", "onDestroy")
     }
 }
--- app/src/main/java/com/jlmillan/sudokumaster/ui/feature/main/MainFragment.kt
+++ app/src/main/java/com/jlmillan/sudokumaster/ui/feature/main/MainFragment.kt
@@ -1,90 +1,64 @@
 package com.jlmillan.sudokumaster.ui.feature.main
 
 import android.os.Bundle
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.jlmillan.sudokumaster.R
+import com.jlmillan.sudokumaster.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                val charactersState by viewModel.stateCharacters.collectAsStateWithLifecycle()
-                val loadingState by viewModel.stateLoading.collectAsStateWithLifecycle()
-                MainView(
-                    loading = loadingState,
-                    characterList = charactersState.first ?: emptyList()
-                )
-                if (charactersState.second != null) {
-                    showError()
-                }
-            }
-        }
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
-        with((activity as? MainActivity)) {
-            this?.showToolbar(true)
-            this?.setToolbarTitle(getString(R.string.characters))
-            updateFavoriteIcon()
-            this?.showProfile(true) {
-                findNavController().navigate(MainFragmentDirections.actionMainFragmentToProfileFragment())
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val composeView: ComposeView = view.findViewById(R.id.compose_view)
+        composeView.setContent {
+            MainScreen(navController = findNavController())
         }
     }
+}
 
-    private fun updateFavoriteIcon() {
-        (activity as? MainActivity)?.showFavorite(viewModel.isFavoriteChecked) {
-            viewModel.isFavoriteChecked = viewModel.isFavoriteChecked.not()
-            updateFavoriteIcon()
-        }
-    }
-
-    private fun showError() {
-        showToast(getString(R.string.error_unknown))
-    }
-
-    @Composable
-    private fun MainView(loading: Boolean, characterList: List<CharacterModel>) {
-        val loadingAlpha = if (loading) 0.5f else 0f
-        LazyColumn(modifier = Modifier.padding(4.dp)) {
-            item {
-                Text("This is a header of characters list")
+@Composable
+fun MainScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                navController.navigate(R.id.action_mainFragment_to_levelFragment)
             }
-            items(items = characterList) { item ->
-                CharacterView(item)
-                Spacer(
-                    modifier = Modifier
-                        .height(1.dp)
-                        .fillMaxWidth()
-                        .background(color = Color.Black)
-                )
-            }
+        ) {
+            Text("Select Level")
         }
-        LoadingView(alpha = loadingAlpha)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate(R.id.action_mainFragment_to_statisticsFragment)
+            }
+        ) {
+            Text("Statistics")
+        }
     }
 }
