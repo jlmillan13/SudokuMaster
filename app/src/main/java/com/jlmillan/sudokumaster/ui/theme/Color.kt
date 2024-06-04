587a13242336f7bbc3bc837045e547517008c6a9
jlmillan13 <jlmillan13@gmail.com>
2024-06-04 20:39:36 +0200

    New drawables, strings and colors

--- app/src/main/java/com/jlmillan/sudokumaster/ui/theme/Color.kt
+++ app/src/main/java/com/jlmillan/sudokumaster/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.jlmillan.sudokumaster.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightAqua = Color(0xFFDDEEF0)
+val PaleBlue = Color(0xFFDCEFFB)
+val WhiteSudoku = Color(0xFFFFFFFF)
+val LavenderBlue = Color(0xFFDFE0EA)
+val VeryPaleCyan = Color(0xFFDEEDF7)
+val MintCream = Color(0xFFE0F1EB)
+val PowderBlue = Color(0xFFDEF0EA)
+val LightMint = Color(0xFFDDEFE7)
+val LightSeafoam = Color(0xFFDAEDE7)
+val VeryLightCyan = Color(0xFFDDEFFB)
+
+val PaleCyan = Color(0xFFA4DBE6)
+val PaleSkyBlue = Color(0xFFA5DCE7)
+val VeryLightBlue = Color(0xFFA5DAE6)
+val LightCyan = Color(0xFFA7DCE7)
+val PaleBlue2 = Color(0xFFA6DDE8)
+val PowderBlue2 = Color(0xFFA6DBE7)
+val AliceBlue = Color(0xFFF7FBFE)
+val LightSkyBlue = Color(0xFFA7DCE5)
+val PaleCyan2 = Color(0xFFA5DBE6)
